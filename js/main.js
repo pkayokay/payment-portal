5c361baa957bc9cc8cb9037f239fe0de946c549b
Paul Kim <pkayokay@gmail.com>
2018-07-07 23:47:44 -0400

    Shorthand v-bind

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 Vue.component('tab-item', {
   props: ['items'],
   template: `
-    <li class="is-active">
+    <li :class="is-active">
     <a class="tabs__link">
       <span class="tabs__icon icon is-small">
-        <i class="fas" v-bind:class="items.icon" aria-hidden="true"></i>
+        <i class="fas" :class="items.icon" aria-hidden="true"></i>
       </span>
       <span>{{ items.text }}</span>
     </a>
@@ -15,10 +15,10 @@ var tabs = new Vue({
   el: '#tabs',
   data: {
     tabList: [
-      { id: 0, text: 'Dashboard', icon: 'fa-tachometer-alt'},
+      { id: 0, text: 'Dashboard', icon: 'fa-tachometer-alt', isActive: true},
       { id: 1, text: 'Payment', icon: 'fa-dollar-sign'},
       { id: 2, text: 'Activity', icon: 'fa-history'},
       { id: 3, text: 'Settings', icon: 'fa-cog'}
-    ]
+    ],
   }
 })
